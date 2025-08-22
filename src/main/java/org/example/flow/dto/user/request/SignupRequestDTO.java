8b8d4af12a80c0fe4e0573f372e3a0072e912cd5
Son <suruna1026@gmail.com>
2025-08-22 17:38:17 +0900

    feat : login,signup

--- src/main/java/org/example/flow/dto/user/request/SignupRequestDTO.java
+++ src/main/java/org/example/flow/dto/user/request/SignupRequestDTO.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.example.flow.entity.Funding;
+import org.example.flow.entity.Place;
 import org.example.flow.entity.User;
 
 @AllArgsConstructor
@@ -17,4 +19,7 @@ public class SignupRequestDTO {
     private String password;
     private User.Role role;
     private String location;
+    private Double latitude;
+    private Double longitude;
+    private Place.Category category;
 }
